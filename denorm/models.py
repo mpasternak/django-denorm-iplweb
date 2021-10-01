3950a35ac3a39851418a72d8a9894ea2cd8febff
Michał Pasternak <michal.dtz@gmail.com>
2021-10-01 17:27:53 +0200

    black, isort, remove __unicode__

--- denorm/models.py
+++ denorm/models.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-from django.db import models
-from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
 
 
 class DirtyInstance(models.Model):
@@ -11,15 +11,13 @@ class DirtyInstance(models.Model):
     DirtyInstance instances are created by the insert/update/delete triggers
     when related objects change.
     """
+
     class Meta:
-        app_label="denorm"
+        app_label = "denorm"
 
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.TextField(blank=True, null=True)
     content_object = GenericForeignKey()
 
     def __str__(self):
-        return u'DirtyInstance: %s,%s' % (self.content_type, self.object_id)
-
-    def __unicode__(self):
-        return u'DirtyInstance: %s, %s' % (self.content_type, self.object_id)
+        return u"DirtyInstance: %s,%s" % (self.content_type, self.object_id)
