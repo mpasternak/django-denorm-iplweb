ee1222117a2d11107685be4aef0690c4d3c6c18e
Michał Pasternak <michal.dtz@gmail.com>
2021-10-01 17:26:52 +0200

    Automatically install triggers
    - automatically install triggers in post_migrate,
    - give an option to disable (from settings.py)

---
+++ denorm/apps.py
@@ -0,0 +1,15 @@
+from django.apps import AppConfig
+from django.conf import settings
+from django.db.models.signals import post_migrate
+
+
+def denorm_install_triggers_after_migrate(using, **kwargs):
+    from denorm import denorms
+
+    denorms.install_triggers(using=using)
+
+
+class DenormAppConfig(AppConfig):
+    def ready(self):
+        if getattr(settings, "DENORM_INSTALL_TRIGGERS_AFTER_MIGRATE", True):
+            post_migrate.connect(denorm_install_triggers_after_migrate, sender=self)
