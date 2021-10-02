808f007582d16b5e6d91d5a61ec85c9ccdc0cf79
Michał Pasternak <michal.dtz@gmail.com>
2021-10-02 07:43:18 +0200

    black reformatting + check for fields in ``skip``

--- denorm/db/base.py
+++ denorm/db/base.py
@@ -1,4 +1,6 @@
-from django.db import models, connections, connection
+from django.core.exceptions import FieldDoesNotExist
+from django.db import connection, connections, models
+
 from ..helpers import remote_field_model
 
 
@@ -43,7 +45,7 @@ class TriggerActionUpdate(TriggerAction):
 
         self.values = []
         for value in values:
-            if hasattr(value, 'sql'):
+            if hasattr(value, "sql"):
                 self.values.append(value.sql())
             else:
                 self.values.append(value)
@@ -57,16 +59,16 @@ def get_fields_with_model(model, meta):
         return [
             (f, f.model if f.model != model else None)
             for f in meta.get_fields()
-            if not f.is_relation
-                or f.one_to_one
-                or (f.many_to_one and f.related_model)
+            if not f.is_relation or f.one_to_one or (f.many_to_one and f.related_model)
         ]
     except AttributeError:
         return meta.get_fields_with_model()
 
-class Trigger(object):
 
-    def __init__(self, subject, time, event, actions, content_type, using=None, skip=None):
+class Trigger(object):
+    def __init__(
+        self, subject, time, event, actions, content_type, using=None, skip=None
+    ):
         self.subject = subject
         self.time = time
         self.event = event
@@ -81,48 +83,77 @@ class Trigger(object):
         else:
             self.connection = connection
 
-        try:
-            from django.contrib.contenttypes.fields import GenericRelation
-        except ImportError:
-            from django.contrib.contenttypes.generic import GenericRelation
+        from django.contrib.contenttypes.fields import GenericRelation
+
+        fields_with_model = None
 
         if isinstance(subject, models.ManyToManyField):
             self.model = None
             self.db_table = subject.m2m_db_table()
-            self.fields = [(subject.m2m_column_name(), ''), (subject.m2m_reverse_name(), '')]
+            self.fields = [
+                (subject.m2m_column_name(), ""),
+                (subject.m2m_reverse_name(), ""),
+            ]
 
         elif isinstance(subject, GenericRelation):
             self.model = None
             self.db_table = remote_field_model(subject)._meta.db_table
-            self.fields = [(k.attname, k.db_type(connection=self.connection)) for k, v in get_fields_with_model(remote_field_model(subject), remote_field_model(subject)._meta) if not v]
-            self.content_type_field = subject.content_type_field_name + '_id'
+            self.fields = [
+                (k.attname, k.db_type(connection=self.connection))
+                for k, v in get_fields_with_model(
+                    remote_field_model(subject), remote_field_model(subject)._meta
+                )
+                if not v
+            ]
+            self.content_type_field = subject.content_type_field_name + "_id"
 
         elif isinstance(subject, models.ForeignKey):
             self.model = subject.model
             self.db_table = self.model._meta.db_table
-            skip = skip or () + getattr(self.model, 'denorm_always_skip', ())
-            self.fields = [(k.attname, k.db_type(connection=self.connection)) for k, v in get_fields_with_model(subject.model, self.model._meta) if not v and k.attname not in skip]
+            skip = skip or () + getattr(self.model, "denorm_always_skip", ())
+            fields_with_model = get_fields_with_model(subject.model, self.model._meta)
+            self.fields = [
+                (k.attname, k.db_type(connection=self.connection))
+                for k, v in fields_with_model
+                if not v and k.attname not in skip
+            ]
 
         elif hasattr(subject, "_meta"):
             self.model = subject
             self.db_table = self.model._meta.db_table
             # FIXME: need to check get_parent_list and add triggers to those
             # The below will only check the fields on *this* model, not parents
-            skip = skip or () + getattr(self.model, 'denorm_always_skip', ())
+            skip = skip or () + getattr(self.model, "denorm_always_skip", ())
+
             self.fields = []
-            try:
-                from django.db.models.fields.related import ForeignObjectRel
-            except:
-                from django.db.models.related import RelatedObject as ForeignObjectRel
-            for k, v in get_fields_with_model(subject, self.model._meta):
+
+            from django.db.models.fields.related import ForeignObjectRel
+
+            fields_with_model = get_fields_with_model(subject, self.model._meta)
+
+            for k, v in fields_with_model:
                 if isinstance(k, ForeignObjectRel):
                     pass
                 else:
                     if not v and k.attname not in skip:
-                        self.fields.append((k.attname, k.db_type(connection=self.connection)))
+                        self.fields.append(
+                            (k.attname, k.db_type(connection=self.connection))
+                        )
 
         else:
             raise NotImplementedError
+
+        if skip and fields_with_model:
+            field_names = [k.attname for k, v in fields_with_model]
+            # Check all fields given as a parameter to skip or denorm_always_skip
+            # to see if they exist:
+            for field_name in skip:
+                if field_name not in field_names:
+                    raise FieldDoesNotExist(
+                        f'Field name "{field_name}", passed as a parameter to "skip" to a trigger for class or '
+                        f'included in class\' "denorm_always_skip" for class {self.subject}, '
+                        f'does not exist. Field names available: {", ".join(field_names)}'
+                    )
 
     def append(self, actions):
         if not isinstance(actions, list):
@@ -132,14 +163,7 @@ class Trigger(object):
             self.actions.append(action)
 
     def name(self):
-        return "_".join([
-            "denorm",
-            self.time,
-            "row",
-            self.event,
-            "on",
-            self.db_table
-        ])
+        return "_".join(["denorm", self.time, "row", self.event, "on", self.db_table])
 
     def sql(self):
         raise NotImplementedError
