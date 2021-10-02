bd434354502fc22ef2b5def2cc49977c26e2e9fe
Michał Pasternak <michal.dtz@gmail.com>
2021-10-02 07:41:10 +0200

    Drop deprecated command

--- denorm/management/commands/denormalize.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand, CommandError
-
-
-class Command(BaseCommand):
-
-    def handle(self, **options):
-        raise CommandError("This management command is deprecated. "
-            "Please consult the documentation for a command reference.")
