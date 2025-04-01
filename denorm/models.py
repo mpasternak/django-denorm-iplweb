6fbf5ae257fff966d9e029716b908c4c67eaacea
Michał Pasternak <michal.dtz@gmail.com>
2025-04-02 01:36:24 +0200

    Fix delete, maybe

--- denorm/models.py
+++ denorm/models.py
@@ -139,19 +139,18 @@ class DirtyInstance(models.Model):
         except klass.DoesNotExist:
             return
 
-    def find_similar(self):
+    def find_similar(self, **kwargs):
         """Find similar objects to this one. Same content_type, same object_id; func_name if this
         object has func_name, but in case of no func name -- find all objects, as no func name
         means even broader scope:"""
         return DirtyInstance.objects.select_for_update(skip_locked=True).filter(
-            content_type=self.content_type,
-            object_id=self.object_id,
+            content_type=self.content_type, object_id=self.object_id, **kwargs
         )
 
     def delete_similar(self):
         """Remove similar DirtyInstances from db, which we haven't yet processed"""
-        self.find_similar().filter(
-            processing_started=None,
+        self.find_similar(processing_started=None).select_for_update(
+            skip_locked=True
         ).delete()
 
     def delete_this_and_similar(self):
