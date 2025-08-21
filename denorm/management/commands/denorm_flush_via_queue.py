529166a8ab191044de2dcc7f0ed69b2c508f9013
Michał Pasternak <michal.dtz@gmail.com>
2025-08-21 11:36:13 +0200

    Push queue flushing to backend

--- denorm/management/commands/denorm_flush_via_queue.py
+++ denorm/management/commands/denorm_flush_via_queue.py
@@ -1,11 +1,13 @@
-from denorms import flush_single
 from django.core.management.base import BaseCommand
-from models import DirtyInstance
+
+from denorm.tasks import flush_via_queue
 
 
 class Command(BaseCommand):
-    help = "Recalculates the value of every denormalized field that was marked dirty."
+    help = (
+        "Recalculates the value of every denormalized field that was marked dirty using "
+        "Celery queues."
+    )
 
     def handle(self, **kwargs):
-        for elem in DirtyInstance.objects.all():
-            flush_single.apply_async(kwargs={"pk": elem.pk}, ignore_result=True)
+        flush_via_queue.apply_async()
