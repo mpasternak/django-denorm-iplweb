3d50f600dea0daf9af3ee01c94c7f154aa62558b
Michał Pasternak <michal.dtz@gmail.com>
2025-10-20 21:01:33 +0200

    bump version 1.7.0 -> 1.8.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="django-denorm-iplweb",
-    version="1.7.0",
+    version="1.8.0",
     description="Denormalization magic for Django",
     long_description="django-denorm-iplweb is a Django application to provide "
     "automatic management of denormalized database fields.",
